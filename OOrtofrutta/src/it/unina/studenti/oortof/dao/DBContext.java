851f9771089cda97cf40f062a077eee84a1acfc4
Valentina <valentin.annunziata@studenti.unina.it>
2021-03-20 17:28:51 +0100

    DBContext

--- OOrtofrutta/src/it/unina/studenti/oortof/dao/DBContext.java
+++ OOrtofrutta/src/it/unina/studenti/oortof/dao/DBContext.java
@@ -1,8 +1,12 @@
 package it.unina.studenti.oortof.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBContext {
     private String url;
@@ -17,6 +21,16 @@ public class DBContext {
 
     public Connection OpenConnection() throws SQLException {
         try {
+          Properties props = new Properties();
+          try (InputStream is = getClass().getResourceAsStream("resources/properties/db.properties")) {
+            props.load(is);
+          }
+          catch (IOException e) {
+            throw new RuntimeException("Exception loading db.properties");
+          }
+          String url = props.getProperty("dburl");
+          String username = props.getProperty("dbuser");
+          String password = props.getProperty("dbpasswd");
             Connection conn = DriverManager.getConnection(url, username, password);
             return conn;
         } catch (SQLException e)
